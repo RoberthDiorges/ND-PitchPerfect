7e5d095a66a25fb5ecbf13ababc7f6530badd00e
Roberth Diorges <roberthdiorges@gmail.com>
2018-01-31 16:03:55 -0200

    adição do appIcon

--- PitchPerfect/Extensions/RecordSoundViewController+Audio.swift
+++ PitchPerfect/Extensions/RecordSoundViewController+Audio.swift
@@ -6,4 +6,94 @@
 //  Copyright © 2018 Roberth Diorges. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import AVFoundation
+
+extension RecordSoundViewController: AVAudioRecorderDelegate {
+    
+    enum StatusButton {
+        case record, stop
+    }
+    
+    // MARK: getDocumentsDirectory
+    func getDocumentsDirectory() -> URL {
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentDirectory = dirPaths[0]
+        let audioFileName = ("newAudio.wav")
+        let pathArray = [documentDirectory, audioFileName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        return filePath!
+    }
+    
+    // MARK: Functions UI
+    func configureUI(_ statusButton: StatusButton) {
+        switch(statusButton) {
+        case .record:
+            setPlayButtonRecordOrStop(record: true)
+            isStopAudio = true
+        case .stop:
+            setPlayButtonRecordOrStop(record: false)
+            isStopAudio = false
+        }
+    }
+    
+    func setPlayButtonRecordOrStop(record:Bool) {
+        if record {
+            let stopImage = UIImage(named: "Stop.png")
+            recordButton.setImage(stopImage, for: .normal)
+            recordAudio()
+        } else {
+            let recordImage = UIImage(named: "Record.png")
+            recordButton.setImage(recordImage, for: .normal)
+            stopRecord()
+        }
+    }
+    
+    // MARK: Permissions func
+    func verifyAccessMicrophone() {
+        switch audioSession.recordPermission() {
+        case AVAudioSessionRecordPermission .granted:
+            print("granted")
+        case AVAudioSessionRecordPermission .denied:
+            displayAlert(title: "Permissão microfone", message: "Por favor, habilite o uso do microfone", titleAction: "Abrir configurações")
+            
+        case AVAudioSessionRecordPermission .undetermined:
+            audioSession.requestRecordPermission({ (granted) in
+                
+            })
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            performSegue(withIdentifier: recordingFilterSegueIdentifier, sender: audioRecorder.url)
+        } else {
+            print("Erro ao gravar...")
+        }
+    }
+    
+    func recordAudio() {
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord, with: AVAudioSessionCategoryOptions .defaultToSpeaker)
+            try audioSession.setActive(true)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        do {
+            try audioRecorder = AVAudioRecorder(url: getDocumentsDirectory(), settings: [:])
+            audioRecorder.isMeteringEnabled = true
+            audioRecorder.delegate = self
+            audioRecorder.prepareToRecord()
+            audioRecorder.record()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func stopRecord() {
+        audioRecorder.stop()
+        try! audioSession.setActive(false)
+    }
+}
+
--- PitchPerfect/ViewControllers/RecordSoundViewController.swift
+++ PitchPerfect/ViewControllers/RecordSoundViewController.swift
@@ -9,120 +9,53 @@
 import UIKit
 import AVFoundation
 
-class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
-  
-  // MARK: - Variables
-  var isStopAudio = false
-  var audioRecorder: AVAudioRecorder!
-  var audioSession = AVAudioSession.sharedInstance()
-  let recordingFilterSegueIdentifier = "recordingFilterSegueIdentifier"
-  
-  // MARK: - Properties
-  @IBOutlet weak var recordButton: UIButton!
-  @IBOutlet weak var statusRecordingLabel: UILabel!
-  
-  // MARK: - Life Cycle
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    verifyAccessMicrophone()
-  }
-  
-  // MARK: - IBActions
-  @IBAction func recordAudioButton_Clicked(_ sender: UIButton) {
-    setupUIButton()
-  }
-  
-  // MARK: - Misc
-  func recordAudio() {
-    do {
-      try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord, with: AVAudioSessionCategoryOptions .defaultToSpeaker)
-      try audioSession.setActive(true)
-    } catch let error {
-      print(error.localizedDescription)
+class RecordSoundViewController: UIViewController {
+    
+    // MARK: - Variables
+    var isStopAudio = false
+    var audioRecorder: AVAudioRecorder!
+    var audioSession = AVAudioSession.sharedInstance()
+    let recordingFilterSegueIdentifier = "recordingFilterSegueIdentifier"
+    
+    // MARK: - Properties
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var statusRecordingLabel: UILabel!
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        verifyAccessMicrophone()
     }
     
-    do {
-      try audioRecorder = AVAudioRecorder(url: getDocumentsDirectory(), settings: [:])
-      audioRecorder.isMeteringEnabled = true
-      audioRecorder.delegate = self
-      audioRecorder.prepareToRecord()
-      audioRecorder.record()
-    } catch {
-      print(error.localizedDescription)
+    // MARK: - IBActions
+    @IBAction func recordAudioButton_Clicked(_ sender: UIButton) {
+        if isStopAudio {
+            configureUI(.stop)
+        } else {
+            configureUI(.record)
+        }
     }
-  }
-  
-  func verifyAccessMicrophone() {
-    switch audioSession.recordPermission() {
-    case AVAudioSessionRecordPermission .granted:
-      print("granted")
-      
-    case AVAudioSessionRecordPermission .denied:
-      displayAlert(title: "Permissão microfone", message: "Por favor, habilite o uso do microfone", titleAction: "Abrir configurações")
-      
-    case AVAudioSessionRecordPermission .undetermined:
-      audioSession.requestRecordPermission({ (granted) in
-      
-      })
+    
+    // MARK: - Misc
+    func displayAlert(title:String, message:String, titleAction:String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let actionOpenConfig = UIAlertAction(title: titleAction, style: .default) { (alertConfig) in
+            if let settings = NSURL(string: UIApplicationOpenSettingsURLString) {
+                UIApplication.shared.open(settings as URL)
+            }
+        }
+        let actionCancel = UIAlertAction(title: "Cancelar", style: .destructive, handler: nil)
+        alertController.addAction(actionOpenConfig)
+        alertController.addAction(actionCancel)
+        present(alertController, animated: true, completion: nil)
     }
-  }
-  
-  func setupUIButton() {
-    if isStopAudio {
-      let stopImage = UIImage(named: "Record.png")
-      recordButton.setImage(stopImage, for: .normal)
-      isStopAudio = false
-      stopRecord()
-    } else {
-      let recordImage = UIImage(named: "Stop.png")
-      recordButton.setImage(recordImage, for: .normal)
-      isStopAudio = true
-      recordAudio()
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == recordingFilterSegueIdentifier {
+            let destinationViewController = segue.destination as! PlaySoundsViewController
+            let recorderAudioURL = sender as! URL
+            destinationViewController.recordedAudioURL = recorderAudioURL
+        }
     }
-  }
-  
-  func displayAlert(title:String, message:String, titleAction:String) {
-    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-    let actionOpenConfig = UIAlertAction(title: titleAction, style: .default) { (alertConfig) in
-      if let settings = NSURL(string: UIApplicationOpenSettingsURLString) {
-        UIApplication.shared.open(settings as URL)
-      }
-    }
-    let actionCancel = UIAlertAction(title: "Cancelar", style: .destructive, handler: nil)
-    alertController.addAction(actionOpenConfig)
-    alertController.addAction(actionCancel)
-    present(alertController, animated: true, completion: nil)
-  }
-  
-  func stopRecord() {
-    audioRecorder.stop()
-    try! audioSession.setActive(false)
-  }
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if segue.identifier == recordingFilterSegueIdentifier {
-      let destinationViewController = segue.destination as! PlaySoundsViewController
-      let recorderAudioURL = sender as! URL
-      destinationViewController.recordedAudioURL = recorderAudioURL
-    }
-  }
-  
-  func getDocumentsDirectory() -> URL {
-    let dirPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
-    let documentDirectory = dirPaths[0]
-    let audioFileName = ("newAudio.wav")
-    let pathArray = [documentDirectory, audioFileName]
-    let filePath = URL(string: pathArray.joined(separator: "/"))
-    return filePath!
-  }
-  
-  //MARK: - AVAudioRecorderDelegate
-  func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-    if flag {
-      performSegue(withIdentifier: recordingFilterSegueIdentifier, sender: audioRecorder.url)
-    } else {
-      print("Erro ao gravar...")
-    }
-  }
 }
 
